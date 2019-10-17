a408fca69bf1c09accf20d2051a43d74162cba32
Yunhao Zhang <zhangyunhao.wangjie@bytedance.com>
2019-10-17 21:59:30 +0800

    release demo

---
+++ precache-manifest.e279772525c9828f4aa1d9f3045f2c98.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28a0c0399a760960369b9f7cd0a0e137",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4907934c333c40ed6494",
+    "url": "/static/css/main.ecbc37d5.chunk.css"
+  },
+  {
+    "revision": "567e4cc5bb3d3a1c92f2",
+    "url": "/static/js/2.aa59a99c.chunk.js"
+  },
+  {
+    "revision": "4907934c333c40ed6494",
+    "url": "/static/js/main.98cf641a.chunk.js"
+  },
+  {
+    "revision": "083bdaf63379cc1b254b",
+    "url": "/static/js/runtime-main.a77fa06e.js"
+  }
+]);
