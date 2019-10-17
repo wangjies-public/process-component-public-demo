b6ac3d90fb571355192388fd6e9a4612371fc092
Yunhao Zhang <zhangyunhao.wangjie@bytedance.com>
2019-10-17 22:05:52 +0800

    demo

---
+++ precache-manifest.f00cfc20390050194da8ac52028e4a66.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d4894a1b5ae5d6cf249ed18916ea456",
+    "url": "/process-component-public-demo/index.html"
+  },
+  {
+    "revision": "34cabf5951949b260b45",
+    "url": "/process-component-public-demo/static/css/main.ecbc37d5.chunk.css"
+  },
+  {
+    "revision": "567e4cc5bb3d3a1c92f2",
+    "url": "/process-component-public-demo/static/js/2.aa59a99c.chunk.js"
+  },
+  {
+    "revision": "34cabf5951949b260b45",
+    "url": "/process-component-public-demo/static/js/main.173daebc.chunk.js"
+  },
+  {
+    "revision": "85a368e7cc5947254f86",
+    "url": "/process-component-public-demo/static/js/runtime-main.9db5ea81.js"
+  }
+]);
